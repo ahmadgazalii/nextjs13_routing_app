d7a4cdb542330bfc6566724e0d3506f2f7e422fd
ahmadgazalii <aghazali489@gmail.com>
2023-03-17 19:20:19 +0500

    next.js changes

--- src/app/user/jan/page.tsx
+++ src/app/user/jan/page.tsx
@@ -1,8 +1,13 @@
-export default ()=>{
+import React from 'react';
+
+export default function MyComponent() {
     return(
-        <div style={{color:'yellow'}}>hello bob</div>
+
+    <div style={{color:'yellow'}}>hello bob</div>
+
     )
-
-
-
-}
+    // component logic here
+  }
+  
+  MyComponent.displayName = 'MyComponent';
+  
